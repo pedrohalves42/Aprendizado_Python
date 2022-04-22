2712415db7b9e567017846a2dce454fccf620d13
ph <pedrohalves42@gmail.com>
2022-04-22 16:49:03 -0300

    'Udemy'

---
+++ Contadores.py
@@ -0,0 +1,4 @@
+contador1 = 0
+contador2 = 0
+for p,r  in enumerate(range(10, 1, -1)):
+    print(p,r,)
---
+++ teste.py
@@ -0,0 +1,4 @@
+nome = input('QUal e o seu nome? ')
+print(nome or 'Voce não digitou nada ')
+
+ 
